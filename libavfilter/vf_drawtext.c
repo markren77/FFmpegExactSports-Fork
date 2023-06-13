a99695c89f2165c70287832062612912c3658a0e
markren77 <mark.ren77@gmail.com>
2023-06-13 12:30:37 -0400

    Removed accidental indentation in line 1041 vf_drawtext.c

--- libavfilter/vf_drawtext.c
+++ libavfilter/vf_drawtext.c
@@ -1038,7 +1038,7 @@ static int func_pts(AVFilterContext *ctx, AVBPrint *bp,
 
 static int func_frame_num(AVFilterContext *ctx, AVBPrint *bp,
                           char *fct, unsigned argc, char **argv, int tag)
-{   
+{
     DrawTextContext *s = ctx->priv;
 
     av_bprintf(bp, "%d", (int)s->var_values[VAR_N]);
